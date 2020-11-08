eb9db9f7eec3ed437144b4f6e6266989d3986162
tommaso <tommasodg55@gmail.com>
2020-11-08 12:25:35 +0100

    es vuoto

--- ChatServerMultithread/src/chatclientmultithread/ChatClient.java
+++ ChatServerMultithread/src/chatclientmultithread/ChatClient.java
@@ -15,34 +15,7 @@ import java.net.*;
 public class ChatClient {
     final static int ServerPort = 1234; 
   
-    public static void main(String args[]) throws UnknownHostException, IOException  
-    { 
-        Scanner scn = new Scanner(System.in); 
-        InetAddress ip = InetAddress.getByName("localhost"); 
-        Socket s = new Socket(ip, ServerPort); 
-        DataInputStream in = new DataInputStream(s.getInputStream()); 
-        DataOutputStream ou = new DataOutputStream(s.getOutputStream());
+    public static void main(String args[]) throws UnknownHostException, IOException { 
         
-        /*public void run() { 
-            while (true) { 
-                String msg = scn.nextLine(); 
-                try { 
-                    ou.writeUTF(msg); 
-                } catch (IOException e) { 
-                    e.printStackTrace(); 
-                } 
-            } 
-        }*/
-
-        /*public void run() {
-            while (true) { 
-                try { 
-                    String msg = in.readUTF(); 
-                    System.out.println(msg); 
-                } catch (IOException e) { 
-                    e.printStackTrace(); 
-                } 
-            } 
-        }*/
     } 
 }
